76c92f38e15a95c2eaea80d87bf22eda144e0933
GeoWill <roper.william@gmail.com>
2021-03-01 11:50:45 +0000

    Update field names

--- scraper.py
+++ scraper.py
@@ -3,37 +3,29 @@ from dc_base_scrapers.xml_scraper import GmlScraper
 
 stations_url = "https://maps.epsom-ewell.gov.uk/getOWS.ashx?MapSource=EEBC/inspire&service=WFS&version=1.1.0&request=GetFeature&Typename=pollingstations"
 stations_fields = {
-    '{http://mapserver.gis.umn.edu/mapserver}ogc_fid': 'ogc_fid',
-    '{http://mapserver.gis.umn.edu/mapserver}objectid': 'objectid',
-    '{http://mapserver.gis.umn.edu/mapserver}psnumber': 'psnumber',
-    '{http://mapserver.gis.umn.edu/mapserver}wardname': 'wardname',
-    '{http://mapserver.gis.umn.edu/mapserver}stationtyp': 'stationtyp',
-    '{http://mapserver.gis.umn.edu/mapserver}uprn': 'uprn',
-    '{http://mapserver.gis.umn.edu/mapserver}address': 'address',
-    '{http://mapserver.gis.umn.edu/mapserver}ward': 'ward',
-    '{http://mapserver.gis.umn.edu/mapserver}easting': 'easting',
-    '{http://mapserver.gis.umn.edu/mapserver}northing': 'northing',
-    '{http://mapserver.gis.umn.edu/mapserver}url': 'url',
+    "{http://mapserver.gis.umn.edu/mapserver}msGeometry": "msGeometry",
+    "{http://mapserver.gis.umn.edu/mapserver}psnumber": "psnumber",
+    "{http://mapserver.gis.umn.edu/mapserver}district": "district",
+    "{http://mapserver.gis.umn.edu/mapserver}address": "address",
+    "{http://mapserver.gis.umn.edu/mapserver}ward": "ward",
 }
 
 districts_url = "https://maps.epsom-ewell.gov.uk/getOWS.ashx?MapSource=EEBC/inspire&service=WFS&version=1.1.0&request=GetFeature&Typename=pollingdistricts"
 districts_fields = {
-    '{http://mapserver.gis.umn.edu/mapserver}ogc_fid': 'ogc_fid',
-    '{http://mapserver.gis.umn.edu/mapserver}objectid': 'objectid',
-    '{http://mapserver.gis.umn.edu/mapserver}wardcode': 'wardcode',
-    '{http://mapserver.gis.umn.edu/mapserver}psnumber': 'psnumber',
-    '{http://mapserver.gis.umn.edu/mapserver}id': 'id',
-    '{http://mapserver.gis.umn.edu/mapserver}uprn': 'uprn',
-    '{http://mapserver.gis.umn.edu/mapserver}comments': 'comments',
-    '{http://mapserver.gis.umn.edu/mapserver}address': 'address',
-    '{http://mapserver.gis.umn.edu/mapserver}shape_area': 'shape_area',
-    '{http://mapserver.gis.umn.edu/mapserver}shape_len': 'shape_len',
+    "{http://www.opengis.net/gml}boundedBy": "boundedBy",
+    "{http://mapserver.gis.umn.edu/mapserver}msGeometry": "msGeometry",
+    "{http://mapserver.gis.umn.edu/mapserver}district": "district",
+    "{http://mapserver.gis.umn.edu/mapserver}pollingplace": "pollingplace",
 }
 
-council_id = 'E07000208'
+council_id = "E07000208"
 
 
-stations_scraper = GmlScraper(stations_url, council_id, 'stations', stations_fields, 'ogc_fid')
+stations_scraper = GmlScraper(
+    stations_url, council_id, "stations", stations_fields, "psnumber"
+)
 stations_scraper.scrape()
-districts_scraper = GmlScraper(districts_url, council_id, 'districts', districts_fields, 'ogc_fid')
+districts_scraper = GmlScraper(
+    districts_url, council_id, "districts", districts_fields, "district"
+)
 districts_scraper.scrape()
